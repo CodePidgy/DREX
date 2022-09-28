3adfea89ae088431c6649a6fd50271dea352deb8
Ruben <rubenjanminjvr@gmail.com>
2022-09-28 14:49:57 +0200

    Update Space Class
    Updated Space Class

--- drex/space.py
+++ drex/space.py
@@ -1,26 +1,25 @@
+from contextlib import redirect_stderr
 import sys
 import pygame
 
 pygame.init()
 
-class Space:
 
+class Space:
     def displayWindow():
 
-        window = pygame.display.set_mode([500,500])
+        window = pygame.display.set_mode([500, 500])
 
-        ball = pygame.image.load("E:\\gitClone\\DREX\\ball.png").convert()
-        
-        ball = pygame.transform.scale(ball, (50,50))    
+        pygame.draw.circle(window, [250, 250, 250, 0], (25, 25), 25)
 
-        window.blit(ball, (1,1))
-        window.blit(ball, (50,50))
-        window.blit(ball, (100,100))
+        pygame.draw.circle(window, [250, 250, 250, 0], (75, 75), 25)
+
+        pygame.draw.circle(window, [250, 250, 250, 0], (150, 150), 25)
 
         pygame.display.flip()
 
         while True:
-            for event in pygame.event.get(): 
+            for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
@@ -28,14 +27,5 @@ class Space:
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         sys.exit()
+
     displayWindow()
-
-
-
-
-
-
-
-
-
-                
